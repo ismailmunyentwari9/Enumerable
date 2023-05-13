e0a3208f6dd8b64c7d18ce97479346d0723311e0
daniel matama <danmatama2020@gmail.com>
2023-05-13 03:47:53 +0300

    Fix linters

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,25 +1,17 @@
 module MyEnumerable
-  # all method
-  def all?
-    array1 = []
-    @list.each { |item| array1.push(item) if yield item }
-    puts array1.length == @list.length
+  def all?(&block)
+    each { |e| return false unless block.call(e) }
+    true
   end
 
-  # any method
-  def any?
-    @list.each do |element|
-      return true if yield element
-    end
+  def any?(&block)
+    each { |e| return true if block.call(e) }
     false
   end
 
-  # filter method
-  def filter
-    array2 = []
-    @list.each do |item|
-      array2.push(item) if yield item
-    end
-    print array2
+  def filter(&block)
+    result = []
+    each { |e| result << e if block.call(e) }
+    result
   end
 end
